9301a5dbedd5fd2c31eeede5ebf91300b06b6b46
brandontzhang <zhangtbrandon2@gmail.com>
2022-04-22 21:34:34 -0400

    removed unused list/listitems

--- router.js
+++ router.js
@@ -1,15 +1,9 @@
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var journalRouter = require('./routes/journals');
-var listRouter = require('./routes/lists');
-var listItemRouter = require('./routes/listItems');
 
 const setupRoutes = (app) => {
   app.use('/', indexRouter);
-  app.use('/users', usersRouter);
   app.use('/journals', journalRouter);
-  app.use('/journals/:journalId/lists', listRouter);
-  app.use('/journals/:journalId/lists/:listId/listItem', listItemRouter);
 }
 
 module.exports = {
